65b06d6231ded25c54b6d5527ec29d7247db9d59
Dominic Westall <dominicwestall@gmail.com>
2024-03-11 14:53:10 +1300

    Added deployment script to create super user on cloud database.

---
+++ myproject/management/commands/createsu.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+ 
+ 
+class Command(BaseCommand):
+ 
+	def handle(self, *args, **options):
+		# Check if a user record with the username 'awsadmin' exists in the database
+		# If it does not exist, then create a super-user with that username,
+		# Supply an email address and a password to it
+		# So, now we have an admin user to manage our AWS RDS database
+		print("WHAT HAPPENS - DOES IT EVEN GET HERE???")
+		if not User.objects.filter(username="awsadmin").exists():
+			User.objects.create_superuser("awsadmin", "dominicwestall@gmail.com", "2021Shades")
